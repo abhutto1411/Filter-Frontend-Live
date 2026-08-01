2591e4ee5cdee86d4a2bb2c6d254cc94ee32ee75
mahnoor <mahnoorazhar70@gmail.com>
2026-08-01 17:35:32 +0500

    first commit

--- src/pages/marketing/marketingRouteUtils.js
+++
@@ -1,8 +0,0 @@
-export function marketingPortalBase(pathname) {
-  if (pathname.includes('/admin/marketing')) return '/admin/marketing';
-  return '/marketing';
-}
-
-export function marketingSectionPath(pathname, section) {
-  return `${marketingPortalBase(pathname)}/${section}`;
-}
