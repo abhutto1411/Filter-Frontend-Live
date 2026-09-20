338996b799b8457fcd28312d8b2f92d6aef705bb
MirzaTaha125 <mirzataha2005@gmail.com>
2026-09-21 00:52:49 +0500

    Dpne

--- src/pages/workshop/WorkshopPurchaseOrdersPage.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import WorkshopPurchaseQuoteDocument from './WorkshopPurchaseQuoteDocument';
-
-export default function WorkshopPurchaseOrdersPage(props) {
-    return <WorkshopPurchaseQuoteDocument mode="orders" {...props} />;
-}
